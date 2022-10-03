414f5b3349a37e4a1aad99c64df0a224d80529e8
Emilse Arias <emilsearias@gmail.com>
2022-10-03 10:37:34 -0300

    instalamos las dependencias necesarias

--- src/App.js
+++
@@ -1,7 +0,0 @@
-function App() {
-  return (
-    <div>Hola mundo</div>
-  );
-}
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <h1 className='display-4'>Hola mundo</h1>
+  );
+}
+
+export default App;
