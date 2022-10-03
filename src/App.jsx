3d94094f9a84d3b928fb30e1547fff67988950ba
Emilse Arias <emilsearias@gmail.com>
2022-10-03 11:37:27 -0300

    agregamos los componentes y las rutas en App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Inicio from './components/views/Inicio';
+import Administrador from './components/views/Administrador';
+import Error404 from './components/views/Error404';
 
 function App() {
   return (
-    <h1 className='display-4'>Hola mundo</h1>
+    // administramos las rutas
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<Inicio></Inicio>}></Route>
+        <Route exact path='/administrar' element={<Administrador></Administrador>}></Route>
+        <Route path='*' element={<Error404></Error404>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/views/Administrador.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Administrador = () => {
+    return (
+        <div>
+        <h1>Esto es la pagina del administrador</h1> 
+        </div>
+    );
+};
+
+export default Administrador;
