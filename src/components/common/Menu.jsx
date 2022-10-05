ccf6b664c39d85f911d1a7e37adc8928929f0dc6
Emilse Arias <emilsearias@gmail.com>
2022-10-05 09:49:03 -0300

    agregamos react-router al menu

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,16 +1,17 @@
-import { Navbar, Nav, Container, NavLink } from "react-bootstrap";
+import { Navbar, Nav, Container} from "react-bootstrap";
+import { Link, NavLink } from "react-router-dom";
 
 const Menu = () => {
   return (
     <Navbar bg="danger" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand to="/">
-          Cafeteria
+        <Navbar.Brand as={Link} to="/">
+          Benito Santos
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <NavLink to="/" className="nav-item nav-link">
+            <NavLink to="/" className='nav-item nav-link'>
               Inicio
             </NavLink>
           <NavLink to="/administrar" className="nav-item nav-link">
